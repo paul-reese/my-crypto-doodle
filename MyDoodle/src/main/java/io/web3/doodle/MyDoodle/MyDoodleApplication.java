2d456eb548e1738990016ec35207098ccfb906e8
paul_reese <paul.reese@gmail.com>
2022-04-05 14:28:34 -0600

    adding spring boot project from start.spring.io

---
+++ MyDoodle/src/main/java/io/web3/doodle/MyDoodle/MyDoodleApplication.java
@@ -0,0 +1,13 @@
+package io.web3.doodle.MyDoodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDoodleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDoodleApplication.class, args);
+	}
+
+}
---
+++ MyDoodle/src/test/java/io/web3/doodle/MyDoodle/MyDoodleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.web3.doodle.MyDoodle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDoodleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
